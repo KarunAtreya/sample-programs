22500d8c77c4f5cdd9a196cd6641c80d6d5741d0
Karun Atreya <33852651+KarunAtreya@users.noreply.github.com>
2018-01-04 12:59:55 +0545

    Add files via upload
    program to ask user a two digit no and print its reversal

---
+++ reverse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int n,firstdigit,seconddigit,reverse;
+printf("enter any two digit no");
+scanf("%d",&n);
+firstdigit=n/10;
+seconddigit=n%10;
+reverse=(seconddigit*10)+firstdigit;
+printf("the reversal no is %d",reverse);
+}
